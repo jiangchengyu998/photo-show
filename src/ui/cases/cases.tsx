700c3ce7893d757e2d4f3df032f5396d0bb30fe5
hanke <jchengyu0829@163.com>
2025-05-31 14:13:43 +0800

    完成case3

--- src/ui/cases/cases.tsx
+++ src/ui/cases/cases.tsx
@@ -7,67 +7,12 @@ import Image from "next/image";
 import { useState } from 'react';
 
 export default function Cases() {
-    const categories = [{
-        id: 1,
-        name: '分类1哈哈',
-        cases: [{
-            id: 1,
-            image: '/images/cases1.png'
-        }, {
-            id: 2,
-            image: '/images/cases1.png'
-        }, {
-            id: 3,
-            image: '/images/cases1.png'
-        }]
-    },
-        {
-            id: 2,
-            name: '分类2哈哈',
-            cases: [{
-                id: 5,
-                image: '/images/cases1.png'
-            }, {
-                id: 6,
-                image: '/images/cases1.png'
-            }]
-        },
-        {
-            id: 3,
-            name: '分类3哈哈',
-            cases: [{
-                id: 7,
-                image: '/images/cases1.png'
-            }, {
-                id: 8,
-                image: '/images/cases1.png'
-            }, {
-                id: 9,
-                image: '/images/cases1.png'
-            }, {
-                id: 14,
-                image: '/images/cases1.png'
-            }]
-        },
-        {
-            id: 4,
-            name: '分类4哈哈',
-            cases: [{
-                id: 10,
-                image: '/images/cases1.png'
-            }, {
-                id: 11,
-                image: '/images/cases1.png'
-            }]
-        },
-        {
-            id: 5,
-            name: '分类5哈哈',
-            cases: [{
-                id: 13,
-                image: '/images/cases1.png'
-            }]
-        },
+    const categories = [
+        { id: 1, name: '分类1哈哈', cases: [{ id: 1, image: '/images/cases1.png' }, { id: 2, image: '/images/cases1.png' }, { id: 3, image: '/images/cases1.png' }] },
+        { id: 2, name: '分类2哈哈', cases: [{ id: 5, image: '/images/cases1.png' }, { id: 6, image: '/images/cases1.png' }] },
+        { id: 3, name: '分类3哈哈', cases: [{ id: 7, image: '/images/cases1.png' }, { id: 8, image: '/images/cases1.png' }, { id: 9, image: '/images/cases1.png' }, { id: 14, image: '/images/cases1.png' }] },
+        { id: 4, name: '分类4哈哈', cases: [{ id: 10, image: '/images/cases1.png' }, { id: 11, image: '/images/cases1.png' }] },
+        { id: 5, name: '分类5哈哈', cases: [{ id: 13, image: '/images/cases1.png' }] },
     ];
 
     const [selectedCategoryId, setSelectedCategoryId] = useState<number>(categories[0].id);
@@ -80,13 +25,13 @@ export default function Cases() {
                 {/* 分类导航栏 */}
                 <div className="cat_nav flex-row">
                     <div className="group_5 flex-col justify-between">
-                        {categories.map((cat, index) => (
+                        {categories.map((cat) => (
                             <div
                                 key={cat.id}
                                 onClick={() => setSelectedCategoryId(cat.id)}
-                                className={`text-wrapper_${index + 2} flex-col cursor-pointer ${selectedCategoryId === cat.id ? 'active' : ''}`}
+                                className={`text-wrapper_3 flex-col cursor-pointer ${selectedCategoryId === cat.id ? 'active' : ''}`}
                             >
-                                <span className={`text_${index + 4}`}>{cat.name}</span>
+                                <span className={`text_5`}>{cat.name}</span>
                             </div>
                         ))}
                     </div>
