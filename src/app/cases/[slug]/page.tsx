b4614da773fb9b17e704a26fe25fbed2cbce2516
hanke <jchengyu0829@163.com>
2025-05-31 21:52:36 +0800

    解决传参问题

--- src/app/cases/[slug]/page.tsx
+++ src/app/cases/[slug]/page.tsx
@@ -4,9 +4,9 @@ export type paramsType = Promise<{ slug: string }>;
 
 export default async function Page(props: { params : paramsType }) {
     // 获取动态路由参数
-    console.log(props);
+    // console.log(props);
     const { slug } = await props.params;
-    console.log(slug);
+    // console.log(slug);
     return (
         <div>
             <CasesDetail slug={slug} />
