e81ff7745814dade2337d54b7f789ed8fea0a348
hanke <jchengyu0829@163.com>
2025-05-31 12:19:00 +0800

    完成case

--- src/ui/cases/cases.tsx
+++ src/ui/cases/cases.tsx
@@ -1,47 +1,42 @@
-import '@/styles/index.cases.css'
-import '@/styles/common.css'
+'use client';
+
+import '@/styles/index.cases.css';
+import '@/styles/common.css';
 import Link from "next/link";
 
 export default function Cases() {
-  return (
-      <div className="page flex-col">
-          <div className="cases_box1 flex-col">
-              <div className="box_8 flex-row">
-                  <div className="group_5 flex-col justify-between">
-                      <div className="text-wrapper_2 flex-col">
-                          <span className="text_4">分类1哈哈</span>
-                      </div>
-                      <div className="text-wrapper_3 flex-col">
-                          <span className="text_5">分类2哈哈</span>
-                      </div>
-                      <div className="text-wrapper_41 flex-col">
-                          <span className="text_6">分类3哈哈</span>
-                      </div>
-                      <div className="text-wrapper_5 flex-col">
-                          <span className="text_7">分类4哈哈</span>
-                      </div>
-                      <div className="text-wrapper_6 flex-col">
-                          <span className="text_8">分类5哈哈</span>
-                      </div>
-                  </div>
-                  <div className="group_6 flex-col justify-between">
+    const categories = [
+        { id: 1, name: '分类1哈哈' },
+        { id: 2, name: '分类2哈哈' },
+        { id: 3, name: '分类3哈哈' },
+        { id: 4, name: '分类4哈哈' },
+        { id: 5, name: '分类5哈哈' },
+    ];
 
-                      <Link href={"/cases/1"}>
-                          <div className="group_7 flex-col"/>
-                      </Link>
-                      <div className="group_8 flex-col"/>
-                  </div>
-                  <div className="group_9 flex-col justify-between">
-                      <div className="group_21 flex-col"/>
-                      <div className="group_3 flex-col"/>
-                  </div>
-              </div>
-              <div className="box_9 flex-row justify-between">
-                  <div className="box_10 flex-col"/>
-                  <div className="box_71 flex-col"/>
-              </div>
-          </div>
-      </div>
-  );
+    return (
+        <div className="page flex-col">
+            <div className="cases_box1 flex-row">
+                <div className="cat_nav flex-row">
+                    <div className="group_5 flex-col justify-between">
+                        {categories.map((cat, index) => (
+                            <Link key={cat.id} href={`/cases/${cat.id}`}>
+                                <div key={cat.id} className={`text-wrapper_${index + 2} flex-col`}>
+                                    <span className={`text_${index + 4}`}>{cat.name}</span>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                </div>
+                <div className="cases_content flex-row justify-between">
+                    <div className="cases_content_box"/>
+                    <div className="cases_content_box"/>
+                    <div className="cases_content_box"/>
+                    <div className="cases_content_box"/>
+                    <div className="cases_content_box"/>
+                    <div className="cases_content_box"/>
+                    <div className="cases_content_box"/>
+                </div>
+            </div>
+        </div>
+    );
 }
-
