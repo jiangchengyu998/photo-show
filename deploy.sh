e01a53caecba9cea68dd7a7df011bb9f64a385ca
hanke <jchengyu0829@163.com>
2025-06-02 20:32:28 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 # 制作镜像
 docker build -t photho-show .
+# 停止并删除旧的容器
+docker stop photho-show || true
 # 删除旧容器
-docker rm -f photho-show
+docker rm -f photho-show || true
 # 启动容器
-docker run -d -p 3000:3000 photho-show
+docker run -d -p 3000:3000 --name photho-show photho-show
